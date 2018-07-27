898069c07c8c54bd96a3d2d64716d58fe4cc8e7e
root <root@instance-1.c.hackxiu.internal>
2018-07-27 08:55:34 +0800

    first commit

--- docker/official/entrypoint.sh
+++ docker/official/entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-CMD=$1
-CONFIG=$2
-
-if [ "$CONFIG" != "" ] && [ "$CMD" == "-c" ]; then
-  echo "$CONFIG" > /etc/v2ray/config.json
-  echo -e "\033[32mUse a custom configuration...\033[0m"
-fi
-
-if [ "$CMD" != "" ] && [ "$CMD" != "-c" ]; then
-  $*
-else
-  v2ray -config /etc/v2ray/config.json
-fi
+sed -i "s/CLIENT_ID/$CLIENT_ID/g" /etc/v2ray/config.json
+sed -i "s/CLIENT_ALTERID/$CLIENT_ALTERID/g" /etc/v2ray/config.json
+sed -i "s#CLIENT_WSPATH#$CLIENT_WSPATH#g" /etc/v2ray/config.json
+v2ray -config=/etc/v2ray/config.json
